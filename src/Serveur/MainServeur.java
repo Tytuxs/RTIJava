fbbdd4a185ca7809867af5d306c11224972994b9
olico <oli.col@hotmail.com>
2022-11-17 16:18:26 +0100

    add comments

--- src/Serveur/MainServeur.java
+++ src/Serveur/MainServeur.java
@@ -9,11 +9,11 @@ public class MainServeur {
         ServeurActivite SR = new ServeurActivite(6000);
 
         Thread t = new Thread(SC);
-        System.out.println("Lancement du ServeurChambre avec un thread");
+        System.out.println("Lancement du ServeurChambre avec un thread sur le port 5056");
         t.start();
 
         Thread t2 = new Thread(SR);
-        System.out.println("Lancement du ServeurReservation avec un thread");
+        System.out.println("Lancement du ServeurReservation avec un thread sur le port 6000");
         t2.start();
     }
 }
